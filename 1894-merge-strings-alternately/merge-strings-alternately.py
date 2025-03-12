b56de2b97b7b589543192744eea56d123253d780
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-03-12 09:45:00 +0530

    Time: 43 ms (22.83%) | Memory: 17.9 MB (33.58%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -11,15 +11,15 @@ class Solution:
         # return result
         i=0
         j=0
-        output = ""
+        result = ""
 
         while i<len(word1) and j<len(word2):
-            output+=word1[i]
-            output+=word2[j]
+            result+=word1[i]
+            result+=word2[j]
             i+=1
             j+=1
         if len(word1)<len(word2):
-            output+=word2[j:]
+            result+=word2[j:]
         else:
-            output+=word1[i:]
-        return output
+            result+=word1[i:]
+        return result
