ec5c816d4782bbfc5a8854a258cb8b0fde6b0b19
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-03-14 22:32:30 +0530

    Time: 7 ms (56.44%) | Memory: 19.2 MB (97.53%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hashMap = {}
+        for i in range(len(nums)):
+            if nums[i] not in hashMap:
+                hashMap[nums[i]] = nums.count(nums[i])
+        
+        max_count = max(hashMap.values())
+
+        for key, value in hashMap.items():
+            if value == max_count and max_count>len(nums)/2:
+                return key
