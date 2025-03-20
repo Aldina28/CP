da29b14e197c067c112495a5cb3b92a2472c1ccd
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-03-20 13:13:00 +0530

    Time: 20 ms (75.51%) | Memory: 23.3 MB (54.43%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -23,22 +23,24 @@ class Solution:
 
         result = []
         product = 1
-        count_zero = 0
+        zero_count = nums.count(0)
+
         for i in range(len(nums)):
             if nums[i]!=0:
                 product*=nums[i]
-            else:
-                count_zero+=1
-        if count_zero > 1:
+            
+        if zero_count == 1:
             for i in range(len(nums)):
-                result.append(0)
-        elif count_zero == 1:
-            for i in range(len(nums)):
-                if nums[i]!=0:
-                    result.append(0)
-                else:
+                if nums[i] == 0:
                     result.append(product)
+                else:
+                    result.append(0)
+            
+        elif zero_count == 0:
+            for i in range(len(nums)):
+                result.append((product//nums[i]))
+        
         else:
             for i in range(len(nums)):
-                result.append(product//nums[i])   
-        return result
+                result.append(0)
+        return result        
