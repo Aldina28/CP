7f7059952aae788832d75aaa56c63afe42c64ffd
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-03-28 20:25:09 +0530

    Time: 9 ms (77.05%) | Memory: 17.8 MB (86.52%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,19 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # if len(s)!=len(t):
-        #     return False
+        if len(s)!=len(t):
+            return False
         
-        # map1={}
-        # map2={}
-        # for i in range(len(s)):
-        #     if s[i] not in map1:
-        #         map1[s[i]] = s.count(s[i])
-        #     if t[i] not in map2:
-        #         map2[t[i]] = t.count(t[i])
-        # if map1==map2:
-        #     return True
-        # else:
-        #     return False
-        sorted_s = sorted(s)
-        sorted_t = sorted(t)
-        return sorted_s == sorted_t
+        map1={}
+        map2={}
+        for i in range(len(s)):
+            if s[i] not in map1:
+                map1[s[i]] = s.count(s[i])
+            if t[i] not in map2:
+                map2[t[i]] = t.count(t[i])
+        if map1==map2:
+            return True
+        else:
+            return False
+        # sorted_s = sorted(s)
+        # sorted_t = sorted(t)
+        # return sorted_s == sorted_t
