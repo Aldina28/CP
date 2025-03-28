b906dfe759625ce9b2909223ae6b2529ff816406
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-03-28 18:33:58 +0530

    Time: 3 ms (97.81%) | Memory: 18.1 MB (35.16%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,25 +1,25 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        hashMap = {}
-        j=0
-        for i in range(len(magazine)):
-            if magazine[i] not in hashMap:
-                hashMap[magazine[i]] = magazine.count(magazine[i])
-        while j<len(ransomNote):
-            if ransomNote[j] in hashMap:
-                hashMap[ransomNote[j]] -= 1
-            else:
-                return False
-            j+=1
-        for key, value in hashMap.items():
-            if value<0:
-                return False
-        return True
-        # if len(magazine) < len(ransomNote):
-        #     return False
+        # hashMap = {}
+        # j=0
+        # for i in range(len(magazine)):
+        #     if magazine[i] not in hashMap:
+        #         hashMap[magazine[i]] = magazine.count(magazine[i])
+        # while j<len(ransomNote):
+        #     if ransomNote[j] in hashMap:
+        #         hashMap[ransomNote[j]] -= 1
+        #     else:
+        #         return False
+        #     j+=1
+        # for key, value in hashMap.items():
+        #     if value<0:
+        #         return False
+        # return True
+        if len(magazine) < len(ransomNote):
+            return False
         
-        # for c in set(ransomNote):
-        #     if magazine.count(c) < ransomNote.count(c): 
-        #         return False 
+        for c in set(ransomNote):
+            if magazine.count(c) < ransomNote.count(c): 
+                return False 
         
-        # return True 
+        return True 
