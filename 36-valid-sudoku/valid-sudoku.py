f457eed217a691124671cdd497e1f006c8d00609
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-03-30 19:42:14 +0530

    Time: 7 ms (38.64%) | Memory: 17.8 MB (43.66%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,14 +1,15 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows=defaultdict(set)
+        rows = defaultdict(set)
         cols = defaultdict(set)
         boxes = defaultdict(set)
 
         for r in range(9):
             for c in range(9):
-                
+
                 if board[r][c] == ".":
                     continue
+                
                 if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in boxes[(r//3,c//3)]:
                     return False
                 
