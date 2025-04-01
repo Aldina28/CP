782c555612de548447a31d04cfe26797ee49eace
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-04-01 10:41:33 +0530

    Time: 3 ms (64.55%) | Memory: 19.2 MB (17.58%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -3,19 +3,17 @@ class Solution:
         stack = []
         result = 0
         for num in tokens:
-            if num == "+" or num=="-" or num=="*"or num=="/":
-                if len(stack)>=2:
-                    operand1 = int(stack.pop())
-                    operand2 = int(stack.pop())
-                    if num == "+":
-                        result = operand1 + operand2
-                    elif num == "-":
-                        result = operand2 - operand1
-                    elif num == "*":
-                        result = operand1*operand2
-                    elif num=="/":
-                        result = int(operand2/operand1)
-                    stack.append(str(result))
-            else:
-                stack.append(num)
-        return int(stack[0])
+            if num.isnumeric() or (num[0]=="-" and len(num)>1):
+                stack.append(int(num))
+            elif len(stack)>=2:
+                operand1 = int(stack.pop())
+                operand2 = int(stack.pop())
+                if num == "+":
+                    stack.append(operand1 + operand2)
+                elif num == "-":
+                    stack.append(operand2 - operand1)
+                elif num == "*":
+                    stack.append(operand1*operand2)
+                elif num=="/":
+                    stack.append(int(operand2/operand1))
+        return stack[0]
