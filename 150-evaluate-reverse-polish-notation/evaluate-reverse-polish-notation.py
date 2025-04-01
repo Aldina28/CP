12dfd879b06ed48ac8213a2842978842f0b3e263
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-04-01 10:45:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (52.56%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,43 +1,34 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        # stack = []
-        # for num in tokens:
-        #     if num.isnumeric() or (num[0]=="-" and len(num)>1):
-        #         stack.append(int(num))
-        #     else:
-        #         operand1 = stack.pop()
-        #         operand2 = stack.pop()
-        #         if num == "+":
-        #             stack.append(operand1 + operand2)
-        #         elif num == "-":
-        #             stack.append(operand2 - operand1)
-        #         elif num == "*":
-        #             stack.append(operand1*operand2)
-        #         elif num=="/":
-        #             stack.append(int(operand2/operand1))
-        # return stack[0]
-        stk = []
-        for i in tokens:
-            if i not in ["+", "-", "/", "*"]:
-                stk.append(i)
+        stack = []
+        for num in tokens:
+            if num.isnumeric() or (num[0]=="-" and len(num)>1):
+                stack.append(int(num))
             else:
-                a = int(stk.pop())
-                b = int(stk.pop())
-                print(a,b)
-                if i=="+":
-                    res = a+b
-                elif i=="-":
-                    res = b-a
-                elif i=="*":
-                    res = a*b
-                elif i=="/":
-                    if b < 0 and a < 0:
-                        res = b//a
-                    elif b < 0 or a < 0:
-                        res = abs(b)//abs(a) * -1
-                    else:
-                        res = b//a
-
-                stk.append(res)
-
-        return int(stk.pop())
+                operand1 = stack.pop()
+                operand2 = stack.pop()
+                if num == "+":
+                    stack.append(operand1 + operand2)
+                elif num == "-":
+                    stack.append(operand2 - operand1)
+                elif num == "*":
+                    stack.append(operand1*operand2)
+                elif num=="/":
+                    stack.append(int(operand2/operand1))
+        return stack[0]
+        # stack = []
+        # for token in tokens:
+        #     if token.isdigit() or (token[0] == '-' and len(token) > 1):
+        #         stack.append(int(token))
+        #     else:
+        #         b = stack.pop()
+        #         a = stack.pop()
+        #         if token == '+':
+        #             stack.append(a + b)
+        #         elif token == '-':
+        #             stack.append(a - b)
+        #         elif token == '*':
+        #             stack.append(a * b)
+        #         elif token == '/':
+        #             stack.append(int(a / b))
+        # return stack[0]
