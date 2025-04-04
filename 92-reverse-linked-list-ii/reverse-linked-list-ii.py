8b0a5b218e28f75f50b421929d36f8450f6c80c5
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-04-04 09:13:45 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (36.72%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -11,17 +11,18 @@ class Solution:
         dummy = ListNode(0, head)
         prev = dummy
 
-        for _ in range(left - 1):
+        for i in range(left-1):
             prev = prev.next
-
-        cur = prev.next
-        for _ in range(right - left):
-            temp = cur.next
-            cur.next = temp.next
+        curr = prev.next
+        for i in range(right-left):
+            temp = curr.next
+            curr.next = temp.next
             temp.next = prev.next
             prev.next = temp
-
         return dummy.next
+
+
+
                     
                     
 
