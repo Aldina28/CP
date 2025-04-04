e21f334e1862967d43985a60bd21c7777b1a73e9
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-04-04 09:57:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (24.95%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -6,24 +6,24 @@
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        # dummy = ListNode(0, head)
-        # prev = dummy
-        # curr = head
-        # while curr:
-        #     if curr.next and curr.next.val==curr.val:
-        #         while curr.next and curr.next.val==curr.val:
-        #             curr = curr.next
-        #         prev.next = curr.next
-        #     else:
-        #         prev = prev.next
-        #     curr = curr.next
-        # return dummy.next
-        if not head or not head.next:
-            return head
-        if head.val==head.next.val:
-            while head.next and head.val==head.next.val:
-                head=head.next
-            return self.deleteDuplicates(head.next)
-        else:
-            head.next = self.deleteDuplicates(head.next)
-            return head
+        dummy = ListNode(0, head)
+        prev = dummy
+        curr = head
+        while curr:
+            if curr.next and curr.next.val==curr.val:
+                while curr.next and curr.next.val==curr.val:
+                    curr = curr.next
+                prev.next = curr.next
+            else:
+                prev = prev.next
+            curr = curr.next
+        return dummy.next
+        # if not head or not head.next:
+        #     return head
+        # if head.val==head.next.val:
+        #     while head.next and head.val==head.next.val:
+        #         head=head.next
+        #     return self.deleteDuplicates(head.next)
+        # else:
+        #     head.next = self.deleteDuplicates(head.next)
+        #     return head
