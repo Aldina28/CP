1149fff74ad6c5ae20ec50db2ebe1d189ed4fa84
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-04-05 21:01:44 +0530

    Time: 16 ms (94.86%) | Memory: 39.4 MB (28.52%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -18,10 +18,11 @@ class Solution:
         while head:
             values.append(head.val)
             head = head.next
-        l, r = 0, len(values)-1
-        while l<r:
-            if values[l]!=values[r]:
-                return False
-            l+=1
-            r-=1
-        return True
+        # l, r = 0, len(values)-1
+        # while l<r:
+        #     if values[l]!=values[r]:
+        #         return False
+        #     l+=1
+        #     r-=1
+        # return True
+        return values == values[::-1]
