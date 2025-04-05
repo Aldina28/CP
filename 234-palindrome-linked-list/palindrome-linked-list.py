66da7dc0a4d8461a3996e5e291754f3d4d8b9f06
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-04-05 21:03:37 +0530

    Time: 35 ms (53.56%) | Memory: 39.5 MB (28.52%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -14,10 +14,10 @@ class Solution:
         # while curr and curr.val == values.pop():
         #     curr = curr.next
         # return curr is None
-        values = []
-        while head:
-            values.append(head.val)
-            head = head.next
+        # values = []
+        # while head:
+        #     values.append(head.val)
+        #     head = head.next
         # l, r = 0, len(values)-1
         # while l<r:
         #     if values[l]!=values[r]:
@@ -25,4 +25,11 @@ class Solution:
         #     l+=1
         #     r-=1
         # return True
-        return values == values[::-1]
+
+        
+        list1 = []
+        while head:
+            list1.append(head.val)
+            head = head.next
+        
+        return list1 == list1[::-1]
