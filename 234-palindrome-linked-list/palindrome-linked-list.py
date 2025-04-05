59f86121d36041c557efc4107e1aeb3c1c036d8c
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-04-05 21:05:17 +0530

    Time: 24 ms (81.28%) | Memory: 39.4 MB (37.83%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -6,12 +6,12 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         
-        # list1 = []
-        # while head:
-        #     list1.append(head.val)
-        #     head = head.next
+        list1 = []
+        while head:
+            list1.append(head.val)
+            head = head.next
         
-        # return list1 == list1[::-1]
+        return list1 == list1[::-1]
 
         # values = []
         # curr = head
@@ -22,14 +22,15 @@ class Solution:
         # while curr and curr.val == values.pop():
         #     curr = curr.next
         # return curr is None
-        values = []
-        while head:
-            values.append(head.val)
-            head = head.next
-        l, r = 0, len(values)-1
-        while l<r:
-            if values[l]!=values[r]:
-                return False
-            l+=1
-            r-=1
-        return True
+
+        # values = []
+        # while head:
+        #     values.append(head.val)
+        #     head = head.next
+        # l, r = 0, len(values)-1
+        # while l<r:
+        #     if values[l]!=values[r]:
+        #         return False
+        #     l+=1
+        #     r-=1
+        # return True
