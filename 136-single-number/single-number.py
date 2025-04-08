5f8715ce5bf20f0d9a85e5547ba6a5c8b45445c3
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-04-08 21:41:06 +0530

    Time: 11 ms (18.42%) | Memory: 19.8 MB (14.31%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,9 +1,14 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        hashMap = {}
-        for i in range(len(nums)):
-            if nums[i] not in hashMap:
-                hashMap[nums[i]] = nums.count(nums[i])
-        for key, values in hashMap.items():
-            if values==1:
-                return key
+        # hashMap = {}
+        # for i in range(len(nums)):
+        #     if nums[i] not in hashMap:
+        #         hashMap[nums[i]] = nums.count(nums[i])
+        # for key, values in hashMap.items():
+        #     if values==1:
+        #         return key
+        HashMap = Counter(nums)
+
+        for item in HashMap:
+            if HashMap[item] == 1:
+                return item
