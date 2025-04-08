7336933c54b6785922af20ac8c4e804b06d6b172
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-04-08 21:43:14 +0530

    Time: 3 ms (65.66%) | Memory: 19.5 MB (77.45%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,5 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        xor = 0
+        for num in nums:
+            xor^=num
+        return xor
         # hashMap = {}
         # for i in range(len(nums)):
         #     if nums[i] not in hashMap:
@@ -7,8 +11,8 @@ class Solution:
         # for key, values in hashMap.items():
         #     if values==1:
         #         return key
-        HashMap = Counter(nums)
+        # HashMap = Counter(nums)
 
-        for item in HashMap:
-            if HashMap[item] == 1:
-                return item
+        # for item in HashMap:
+        #     if HashMap[item] == 1:
+        #         return item
