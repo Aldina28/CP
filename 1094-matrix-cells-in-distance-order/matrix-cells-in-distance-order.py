6416fe0e38446d0924b8184ffecda11a1eb631bb
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-04-17 16:26:27 +0530

    Time: 15 ms (57.35%) | Memory: 19.4 MB (56.72%) - LeetSync

--- 1094-matrix-cells-in-distance-order/matrix-cells-in-distance-order.py
+++ 1094-matrix-cells-in-distance-order/matrix-cells-in-distance-order.py
@@ -1,20 +1,8 @@
 class Solution:
     def allCellsDistOrder(self, rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
-        distance_dict = {}
+        res = []
         for r in range(rows):
             for c in range(cols):
-                distance = abs(r - rCenter) + abs(c - cCenter)
-                if distance in distance_dict:
-                    distance_dict[distance].append([r, c])
-                else:
-                    distance_dict[distance] = [[r, c]]
-        
-        # Sort the distance_dict
-        sorted_keys = sorted(distance_dict.keys())
-        sorted_dict = {x: distance_dict[x] for x in sorted_keys}
-
-        # Use sorted_dict to return the final answer
-        ans = []
-        for value in sorted_dict.values():
-            ans.extend(value)
-        return ans
+                res.append([r,c])
+        res.sort(key=lambda x: abs(x[0] - rCenter) + abs(x[1] - cCenter))
+        return res
