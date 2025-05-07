a7f6630449ca39f08481cdccba0abdf7f4892b6d
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-07 12:59:27 +0530

    Time: 120 ms (87.19%) | Memory: 18.8 MB (12.26%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.py
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.py
@@ -1,19 +1,21 @@
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
-        n = len(moveTime)
-        m = len(moveTime[0])
+        n, m = len(moveTime), len(moveTime[0])
+
+        if n == 1 and m == 1:
+            return 0
 
         visited = set()
         visited.add((0, 0))
-        heap = [(0, 0, 0)]
 
-        while heap:
-            t, x, y = heappop(heap)
-            if (x, y) == (n-1, m-1):
+        h = [(0, 0, 0)]
+        while h:
+            t, x, y = heappop(h)
+            if (x, y) == (n - 1, m - 1):
                 return t
-            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0,1)]:
-                u, v = dx+x, dy+y
-                if 0<=u<n and 0<=v<m and (u, v) not in visited:
-                    heappush(heap, (max(t, moveTime[u][v])+1, u, v))
+            for dx, dy in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+                u, v = x + dx, y + dy
+                if 0 <= u < n and 0 <= v < m and (u, v) not in visited:
+                    heappush(h, (max(t, moveTime[u][v]) + 1, u, v))
                     visited.add((u, v))
         return -1
