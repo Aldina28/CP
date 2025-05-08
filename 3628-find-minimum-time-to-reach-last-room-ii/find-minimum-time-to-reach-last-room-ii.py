e1cb8b8ce520b6f08578f1dc3baf240ef358236a
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-08 10:40:29 +0530

    Time: 363 ms (97.00%) | Memory: 72.3 MB (97.00%) - LeetSync

--- 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.py
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.py
@@ -1,23 +1,33 @@
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
-        n, m = len(moveTime), len(moveTime[0])
-        if n==1 or m==1:
-            return 0
-        visited = set()
-        visited.add((0, 0))
-        h=[(0, 0, 0, 0)]
+        rows = len(moveTime)
+        cols = len(moveTime[0])
+
+        dist = [[float('inf')]*cols for _ in range(rows)]
+
+        dist[0][0] = 0
+
+        h = [(0,0,0,2)]
+
+        moves = [(0,1),(1,0),(-1,0),(0,-1)]
+
         while h:
-            t, x, y, step = heappop(h)
-            if (x, y) == (n-1, m-1):
-                return t
-            if (x, y, step % 2) in visited:
-                continue
-            visited.add((x, y, step % 2))
-            for dx, dy in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
-                u, v = dx+x, dy+y
-                if 0<=u<n and 0<=v<m:
-                    move_cost = 1 if step % 2 == 0 else 2
-                    depart_time = max(t, moveTime[u][v])
-                    arrival_time = depart_time + move_cost
-                    heappush(h, (arrival_time, u, v, step + 1))
-        return -1
+            time, r, c, prev = heappop(h)
+            for dx, dy in moves:
+                xx, yy = r+dx, c+dy
+
+                if (xx<0 or xx>=rows or yy<0 or yy>=cols):
+                    continue
+                
+                movecost = 2
+                if prev==2:
+                    movecost = 1
+
+                
+                new_time = max(moveTime[xx][yy], dist[r][c]) + movecost
+
+                if new_time < dist[xx][yy]:
+                    if (xx==rows-1 and yy==cols-1):
+                        return new_time
+                    dist[xx][yy] = new_time
+                    heappush(h,(new_time, xx, yy, movecost))
