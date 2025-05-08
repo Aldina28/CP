0edcf42e087ae3c9633fc234b7222ab3e435723f
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-08 09:43:25 +0530

    Time: 84 ms (99.87%) | Memory: 83.3 MB (96.48%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,16 +1,18 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        if [source, destination] in edges or [destination, source] in edges:
+        if [source,destination] in edges or [destination,source] in edges: 
             return True
-        roots = list(range(n))
 
-        def find(u):
-            if u == roots[u]:
-                return u
-            roots[u] = find(roots[u])
-            return roots[u]
-        
-        for u, v in edges:
-            roots[find(u)] = find(v)
+        graph = list(range(n))
+
+        def find(n):
+            if graph[n] != n:
+                graph[n] = find(graph[n])
+
+            return graph[n]
+
+        for u,v in edges:
+            graph[find(u)] = find(v)
+
         return find(source) == find(destination)
         
