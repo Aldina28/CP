87f3c4908f8be701e822c20507c3276550af43c7
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-09 12:54:56 +0530

    Time: 246 ms (61.54%) | Memory: 19.9 MB (95.66%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,28 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        islands = 0
-        visited = set()
-        rows, cols = len(grid), len(grid[0])
-
-        def bfs(r, c):
-            q = deque()
-            visited.add((r, c))
-            q.append((r, c))
-
-            while q:
-                row, col = q.popleft()
-                directions = [[1,0],[-1,0],[0,1],[0,-1]]
-
-                for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if 0 <= r < rows and 0 <= c < cols and grid[r][c] == "1" and (r, c) not in visited:
-                        q.append((r, c))
-                        visited.add((r, c))
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visited:
-                    islands += 1
-                    bfs(r, c)
-
-        return islands
+        q = deque()
+        count = 0
+        moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for a in range(len(grid)):
+            for b in range(len(grid[0])):
+                if grid[a][b] == '1':
+                    count+=1
+                    q.append((a, b))
+                    grid[a][b] = '0'
+                    while q:
+                        i, j = q.popleft()
+                        for dx, dy in moves:
+                            x, y = i+dx, j+dy
+                            if ((0 <= x < len(grid)) and (0<= y < len(grid[0]))):
+                                if grid[x][y] == "1":
+                                    q.append((x, y))
+                                    grid[x][y]="0"
+        return count
