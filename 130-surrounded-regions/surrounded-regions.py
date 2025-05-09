fa0fc620f4afaa64fc864246ffeab91c3680fe18
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-09 13:59:48 +0530

    Time: 7 ms (63.39%) | Memory: 21.9 MB (47.38%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.py
@@ -0,0 +1,37 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        if not board or not board[0]:
+            return
+        rows = len(board)
+        cols = len(board[0])
+        queue = deque()
+        moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        def bfs(r, c):
+            queue.append((r, c))
+            board[r][c] = 'T'
+            while queue:
+                i, j = queue.popleft()
+                for dx, dy in moves:
+                    x, y = i + dx, j + dy
+                    if 0 <= x < rows and 0 <= y < cols and board[x][y] == 'O':
+                        queue.append((x, y))
+                        board[x][y] = 'T'
+        for r in range(rows):
+            if board[r][0] == 'O':
+                bfs(r, 0)
+            if board[r][cols-1] == 'O':
+                bfs(r, cols-1)
+        for c in range(cols):
+            if board[0][c] == 'O':
+                bfs(0, c)
+            if board[rows-1][c] == 'O':
+                bfs(rows-1, c)
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                elif board[r][c] == 'T':
+                    board[r][c] = 'O'
