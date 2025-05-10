3cfaa6c9c71fa00ef828482ce02308875dfcb617
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-10 14:53:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (16.68%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -3,30 +3,24 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         courses = prerequisites
         g = defaultdict(list)
-
-        for a, b in courses:
+        for a, b  in courses:
             g[a].append(b)
-        
         unvisited = 0
         visiting = 1
         visited = 2
-        states = [unvisited] * numCourses
-
+        states = [unvisited]*numCourses
         def dfs(node):
-            # state = states[node]
-            if states[node] == visited: return True
+            if states[node]==visited: return True
             elif states[node] == visiting: return False
 
             states[node] = visiting
 
-            for nei in g[node]:
-                if dfs(nei) == False:
+            for neighbor in g[node]:
+                if dfs(neighbor) == False:
                     return False
-            
+
             states[node] = visited
-        
         for i in range(numCourses):
-            if dfs(i) == False:
+            if dfs(i)==False:
                 return False
         return True
-        
