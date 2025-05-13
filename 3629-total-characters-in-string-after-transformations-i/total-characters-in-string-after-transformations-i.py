a2f4054c3bdc94a4ba1da8d962b8ee6b82acbcdb
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-13 15:18:22 +0530

    Time: 55 ms (99.35%) | Memory: 22.1 MB (24.84%) - LeetSync

--- 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.py
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.py
@@ -1,8 +1,9 @@
+dp = [1] * (100_000 + 26)
+for i in range(26, len(dp)):
+    dp[i] = (dp[i-26] + dp[i-25]) % 1_000_000_007
+        
 class Solution:
     def lengthAfterTransformations(self, s: str, t: int) -> int:
-        dp = [1] * (100_000 + 26)
-        for i in range(26, len(dp)):
-            dp[i] = (dp[i-26] + dp[i-25]) % 1_000_000_007
         
         ans = 0
         cnt = Counter(s)
