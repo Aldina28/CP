ec924a636f46c4f36e126bfceb26b2bcaacf2e6d
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-19 16:56:14 +0530

    Time: 1 ms (9.30%) | Memory: 17.9 MB (14.30%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -6,10 +6,10 @@ class Solution:
         pivot = len(nums)-1
         while pivot>0 and nums[pivot-1]>=nums[pivot]:
             pivot-=1
-        if pivot==0:
-            nums.reverse()
-            return
-        i=len(nums)-1
+            if pivot == 0:
+                nums.reverse()
+                return 
+        i = len(nums)-1
         while i>=pivot and nums[i]<=nums[pivot-1]:
             i-=1
         nums[i], nums[pivot-1] = nums[pivot-1], nums[i]
