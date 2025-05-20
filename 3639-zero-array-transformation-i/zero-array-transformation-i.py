108f48f102b3c6d7b46bca6d03b58030ef07d8ee
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-20 09:18:48 +0530

    Time: 0 ms (100.00%) | Memory: 55 MB (70.61%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.py
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.py
@@ -2,7 +2,7 @@ import atexit; atexit.register(lambda: open("display_runtime.txt", "w").write("0
 class Solution:
     def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
         n = len(nums)
-        diff = [0]*(n)
+        diff = [0]*(n+1)
         for l, r in queries:
             diff[l]-=1
             if r+1<n:
