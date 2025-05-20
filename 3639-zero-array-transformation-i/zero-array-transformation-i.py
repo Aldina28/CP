ab4621db01e456660fd7fbae2d5677ffefc63c9a
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-20 09:03:48 +0530

    Time: 115 ms (19.04%) | Memory: 55 MB (52.93%) - LeetSync

---
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        n = len(nums)
+        diff = [0]*(n+1)
+        for l, r in queries:
+            diff[l]-=1
+            if r+1<n:
+                diff[r+1] += 1
+        decrement = 0
+        for i in range(n):
+            decrement+=diff[i]
+            nums[i] = max(nums[i]+decrement, 0)
+        return nums.count(0) == len(nums)
