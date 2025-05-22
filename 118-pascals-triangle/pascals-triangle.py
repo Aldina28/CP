b29f7ca8de02a7dddc53d3c503e1da8d7a0e5301
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-22 21:46:12 +0530

    Time: 1 ms (14.84%) | Memory: 17.7 MB (87.15%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,10 +1,9 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        liist=[]
+        result = []
         for i in range(numRows):
-            row=[1]*(i+1)
-            for j in range(1,i):
-                row[j]=liist[i-1][j-1]+liist[i-1][j]
-            liist.append(row)
-        return liist
-        
+            row = [1]*(i+1)
+            for j in range(1, i):
+                row[j] = result[i-1][j-1]+result[i-1][j]
+            result.append(row)
+        return result
