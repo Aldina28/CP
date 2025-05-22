02bf681ce5d9a1757438c531fff0b7c3f8804dee
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-22 07:20:07 +0530

    Time: 15 ms (97.03%) | Memory: 23 MB (98.00%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,35 +1,22 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # result = []
-        # product = 1
-        # for i in range(len(nums)):
-        #     l=i-1
-        #     r=i+1
-
-        #     while l>=0 and r<= len(nums)-1:
-        #         product *= (nums[l]*nums[r])
-        #         l-=1
-        #         r+=1
-        #     while l>=0:
-        #             product*=nums[l] 
-        #             l-=1
-
-        #     while r<=len(nums)-1:
-        #             product*=nums[r]
-        #             r+=1
-        #     result.append(product)
-        #     product = 1
-        # return result
-
-        output = [1]*len(nums)
-        l=1
+        zero_count = nums.count(0)
+        total_product = 1
+        result = []
         for i in range(len(nums)):
-            output[i] *= l
-            l*=nums[i]
-
-        r=1
-        for i in range(len(nums)-1, -1, -1):
-            output[i]*=r  
-            r*=nums[i]
-        
-        return output
+            if nums[i]!=0:
+                total_product *= nums[i]
+        if zero_count==0:
+            for i in range(len(nums)):
+                result.append(total_product//nums[i])
+        elif zero_count==1:
+            for i in range(len(nums)):
+                if nums[i]!=0:
+                    result.append(0)
+                else:
+                    result.append(total_product)
+        elif zero_count>1:
+            for i in range(len(nums)):
+                result.append(0)
+        return result
+                 
