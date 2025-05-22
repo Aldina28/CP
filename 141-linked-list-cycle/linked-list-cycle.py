8b7568a2b8d5808acfdc6fbf7daf5b6dd1919353
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-22 19:26:34 +0530

    Time: 41 ms (91.16%) | Memory: 19.8 MB (56.48%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -3,7 +3,7 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if not head:
@@ -12,6 +12,6 @@ class Solution:
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-            if slow == fast:
+            if slow==fast:
                 return True
         return False
