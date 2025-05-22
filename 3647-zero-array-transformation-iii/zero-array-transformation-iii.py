fb806d3ee1f145ef2ac5c3061710f79eee90095c
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-22 22:15:15 +0530

    Time: 138 ms (96.51%) | Memory: 61.5 MB (67.69%) - LeetSync

--- 3647-zero-array-transformation-iii/zero-array-transformation-iii.py
+++ 3647-zero-array-transformation-iii/zero-array-transformation-iii.py
@@ -1,27 +1,19 @@
 class Solution:
     def maxRemoval(self, nums: List[int], queries: List[List[int]]) -> int:
-        n, q = len(nums), len(queries)
-        starts = [[] for _ in range(n)]
-        for l, r in queries:
-            starts[l].append(r)
+        n, edges = len(nums), defaultdict(list)
+        for start, end in queries:
+            edges[start].append(end)
 
-        avail = []   # max‐heap of ends (store negatives)
-        active = []  # min‐heap of ends
-        chosen = 0
-        for i in range(n):
-            for r in starts[i]:
-                heapq.heappush(avail, -r)
+        lines, heap, current = [0] * (n + 1), [], 0
+        for num_index, num in enumerate(nums):
+            if num_index in edges:
+                for end in edges[num_index]: heappush(heap, -end)
 
-            while active and active[0] < i:
-                heapq.heappop(active)
+            current += lines[num_index]
+            while current < num:
+                if not heap or -heap[0] < num_index: return -1
+                end = -heappop(heap)
+                current += 1
+                lines[end + 1] -= 1
 
-            need = nums[i] - len(active)
-            for _ in range(need):
-                while avail and -avail[0] < i:
-                    heapq.heappop(avail)
-                if not avail:
-                    return -1
-                r = -heapq.heappop(avail)
-                heapq.heappush(active, r)
-                chosen += 1
-        return q - chosen
+        return len(heap)
