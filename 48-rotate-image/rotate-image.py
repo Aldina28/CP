155819201e34d2d8b65c78acb0b846e1f94f5412
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-22 22:41:14 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (23.05%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -3,19 +3,12 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        edge_len = len(matrix)
-        top = 0 
-        bottom = edge_len-1
-        while top<bottom:
-            for col in range(edge_len):
-                temp = matrix[top][col]
-                matrix[top][col] = matrix[bottom][col]
-                matrix[bottom][col] = temp
-            top+=1
-            bottom-=1
-        for row in range(edge_len):
-            for col in range(row+1, edge_len):
-                temp = matrix[row][col]
-                matrix[row][col] = matrix[col][row]
-                matrix[col][row] = temp
-        return matrix
+        l = 0
+        r = len(matrix)-1
+        while l<r:
+            matrix[l], matrix[r] = matrix[r], matrix[l]
+            r-=1
+            l+=1
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
