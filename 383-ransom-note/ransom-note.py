c30aadb65fef69a3a5c7478570adf50d6f82ea8f
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-22 10:03:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (82.07%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,8 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        if len(ransomNote)>len(magazine):
-            return False
-        for i in set(ransomNote):
-            if magazine.count(i) < ransomNote.count(i):
-                return False
+        for i in ransomNote:
+            if i in magazine:
+                magazine = magazine.replace(i, '', 1)
+            else: return False
         return True
