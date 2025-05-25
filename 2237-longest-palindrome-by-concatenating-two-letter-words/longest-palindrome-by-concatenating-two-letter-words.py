cceb597295cc807de497333a6c0bcb139a25df22
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-25 19:43:00 +0530

    Time: 49 ms (72.38%) | Memory: 35.8 MB (12.27%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,16 +1,16 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
-        freq=Counter(words)
-        pal=0
-        hasDouble=False
-        for key, val in freq.items():
-            if key[0]==key[1]:
-                pal+=val//2*4
-                if val&1: hasDouble=True
-            elif key[0]<key[1]:
-                rev=key[1]+key[0]
-                pal+=min(val, freq[rev] )*4
-        return pal+hasDouble*2
-        
+        count = Counter(words)
+        res = 0
+        center = False
+        for w in list(count.keys()):
+            rev = w[::-1]
+            if w == rev:
+                res += (count[w]//2)*2
+                if count[w]%2:
+                    center = True
+            elif w<rev:
+                res+=2*min(count[w], count[rev])
+        return (res+(1 if center else 0))*2
     
            
