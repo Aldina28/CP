2b2fe11b8007525145b3e8962e4e2abf85ce57b0
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-05-30 08:55:16 +0530

    Time: 54 ms (23.69%) | Memory: 19.2 MB (77.06%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
@@ -8,21 +8,11 @@ class Node:
 
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
-        if not root:
-            return []
-
-        res = []
-
-        # Define the DFS function
-        def dfs(root):
-            # Recursively call dfs for each child of the current node
-            for x in root.children:
-                dfs(x)
-            # Append the value of the current node to the result list
-            res.append(root.val)
-
-        # Start DFS from the root
-        dfs(root)
-
-        # Return the result list containing node values in post-order
-        return res
+        def dfs(root, output):
+            if not root:
+                return 
+            for child in root.children:
+                dfs(child, output)
+            output.append(root.val)
+            return output
+        return dfs(root, [])
