e91ea159cdabb42df1df8cecbe48e3f83aa43a98
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-02 10:44:34 +0530

    Time: 68 ms (86.35%) | Memory: 29.7 MB (14.14%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -3,14 +3,13 @@ class Solution:
         count = 0
         temp = 0
         l = 0
-        maximum = max(nums)
+        maximum_elt = max(nums)
         for right in nums:
-            if right == maximum:
+            if right==maximum_elt:
                 temp+=1
             while temp>=k:
-                if nums[l]==maximum:
+                if nums[l] == maximum_elt:
                     temp-=1
                 l+=1
             count+=l
         return count
-
