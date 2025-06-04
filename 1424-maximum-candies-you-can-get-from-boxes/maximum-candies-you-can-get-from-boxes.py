8b9de17a75f3dccfb93106632e4fc3ede45a07d9
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-04 07:04:28 +0530

    Time: 15 ms (79.36%) | Memory: 28 MB (86.83%) - LeetSync

--- 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.py
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.py
@@ -1,30 +1,24 @@
 class Solution:
     def maxCandies(self, status: List[int], candies: List[int], keys: List[List[int]], containedBoxes: List[List[int]], initialBoxes: List[int]) -> int:
-        queue = deque(initialBoxes)
-        n = len(status)
-        visited = [False] * n
-        hasBox = [False] * n
-        hasKey = status[:]
         count = 0
-
-        for box in initialBoxes:
-            hasBox[box] = True
-
+        queue = deque(initialBoxes)
+        visited = set()
         while queue:
-            box = queue.popleft()
-            if visited[box] or not hasKey[box]:
-                continue
-
-            visited[box] = True
-            count += candies[box]
-
-            for b in containedBoxes[box]:
-                hasBox[b] = True
-                queue.append(b)
-
-            for k in keys[box]:
-                hasKey[k] = 1
-                if hasBox[k]:
-                    queue.append(k)
-
+            for i in range(len(queue)):
+                box = queue.popleft()
+                if box in visited:
+                    continue
+                visited.add(box)
+                if status[box]:
+                    count+=candies[box]
+                    for k in keys[box]:
+                        if k in visited and status[k] == 0:
+                            status[k] = 1
+                            queue.append(k)
+                            visited.remove(k)
+                        else:
+                            status[k] = 1
+                    for extra_box in containedBoxes[box]:
+                        queue.append(extra_box)
         return count
+       
