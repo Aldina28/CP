cfcf6c58d795a599d5d3cc2b37b1825826eb6d8c
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-07 10:52:05 +0530

    Time: 3 ms (48.09%) | Memory: 17.8 MB (65.01%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -10,5 +10,6 @@ class Solution:
         dp = [0]*(n+1)
         dp[0], dp[1], dp[2], dp[3] = 1, 1, 2, 5
         for i in range(4, n+1):
-            dp[i] = (dp[i-1]*2 + dp[i-3])%mod
+            dp[i] = (dp[i-1]*2 + dp[i-3]) % mod
+
         return dp[n]
