9385a293b2b4a51a140a19ca6a70ce178b9f8221
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-07 13:54:23 +0530

    Time: 7 ms (47.31%) | Memory: 19.6 MB (22.19%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,28 +1,24 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        courses = prerequisites
-        g = defaultdict(list)
-        for a, b in prerequisites:
-            g[a].append(b)
-        unvisited = 0
-        visited = 2
-        visiting = 1
+        preMap = {i:[] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+        visited = set()
 
-        states = [unvisited]*numCourses
-
-        def dfs(node):
-            if states[node] == visited: return True
-            if states[node] == visiting: return False
-
-            states[node] = visiting
-
-            for neighbor in g[node]:
-                if dfs(neighbor) == False:
+        def dfs(crs):
+            if preMap[crs] == []:
+                return True
+            if crs in visited:
+                return False
+            visited.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
                     return False
-            states[node] = visited
-
-        for i in range(numCourses):
-            if dfs(i) == False:
+            visited.remove(crs)
+            preMap[crs] = []
+            return True
+        for crs in range(numCourses):
+            if not dfs(crs):
                 return False
         return True
+                            
