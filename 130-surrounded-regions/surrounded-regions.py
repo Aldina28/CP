f0d3bf0af4cbcfd7114303afff2fe8bddd35cbed
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-07 17:11:09 +0530

    Time: 8 ms (45.47%) | Memory: 21.9 MB (51.43%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,38 +1,35 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        if not board or not board[0]:
-            return
-        rows = len(board)
-        cols = len(board[0])
+        rows, cols = len(board), len(board[0])
         queue = deque()
-        moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1) ]
         def bfs(r, c):
             queue.append((r, c))
             board[r][c] = 'T'
             while queue:
-                i, j = queue.popleft()
-                for dx, dy in moves:
-                    x, y = i + dx, j + dy
-                    if 0 <= x < rows and 0 <= y < cols and board[x][y] == 'O':
-                        queue.append((x, y))
-                        board[x][y] = 'T'
-        for r in range(rows):
-            if board[r][0] == 'O':
-                bfs(r, 0)
-            if board[r][cols-1] == 'O':
-                bfs(r, cols-1)
-        for c in range(cols):
-            if board[0][c] == 'O':
-                bfs(0, c)
-            if board[rows-1][c] == 'O':
-                bfs(rows-1, c)
+                row, col = queue.popleft()
+                for dx, dy in directions:
+                    r, c = row+dx, col+dy
+                    if r in range(rows) and c in range(cols) and board[r][c] == 'O':
+                        queue.append((r,c))
+                        board[r][c] = 'T'
+        for row in range(rows):
+            if board[row][cols-1] == 'O':
+                bfs(row, cols-1)
+            if board[row][0] == 'O':
+                bfs(row, 0)
+        for col in range(cols):
+            if board[0][col] == 'O':
+                bfs(0, col)
+            if board[rows-1][col] == 'O':
+                bfs(rows-1, col)
+        
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] == 'O':
                     board[r][c] = 'X'
-                elif board[r][c] == 'T':
+                if board[r][c] == 'T':
                     board[r][c] = 'O'
