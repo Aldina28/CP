29bda1e0599b1363ed0d6214e185b338dab09f90
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-10 10:06:23 +0530

    Time: 26 ms (89.16%) | Memory: 20.7 MB (30.67%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        adj_list = defaultdict(list)
+        for pre, crs in prerequisites:
+            adj_list[crs].append(pre)
+        preMap = {}
+        result = []
+        def dfs(crs):
+            if crs not in preMap:
+                preMap[crs] = set()
+                for pre in adj_list[crs]:
+                    preMap[crs] |= dfs(pre)
+                preMap[crs].add(crs)
+            return preMap[crs]
+        for crs in range(numCourses):
+            dfs(crs)
+        for u, v in queries:
+            result.append(u in preMap[v])
+        return result
+
+        
