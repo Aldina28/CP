d49c49b248dc051ba7ec31ce11b7ac546a0dc083
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-13 18:41:07 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (5.48%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,11 +1,11 @@
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        res,curr_sum = 0,0
+        res, curr_sum = 0, 0
         data = defaultdict(int)
-        data[0] = 1
-        for i,num in enumerate(nums):
-            curr_sum += num
-            res += data[curr_sum-k]
-            data[curr_sum] += 1
+        data[0]=1
+        for num in nums:
+            curr_sum+=num
+            res+=data[curr_sum-k]
+            data[curr_sum]+=1
         return res
