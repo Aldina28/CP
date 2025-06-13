40eaa84ef105e4ef0573fcc9c724e66d80e22712
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-13 13:17:17 +0530

    Time: 459 ms (25.10%) | Memory: 32.7 MB (11.74%) - LeetSync

--- 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.py
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.py
@@ -1,22 +1,26 @@
 class Solution:
     def minimizeMax(self, nums: List[int], p: int) -> int:
         nums.sort()
-
-        def isValid(diff):
-            cnt,i=0,1
-            while i<len(nums):
-                if nums[i]-nums[i-1]<=diff:
-                    cnt+=1
+        
+        def isValid(mid):
+            count, i = 0, 0
+            while i<len(nums)-1:
+                if abs(nums[i]-nums[i+1])<=mid:
+                    count+=1
+                    i+=2
+                else:
                     i+=1
-                i+=1
-            return cnt>=p
-            
-        l,r=0,nums[-1]-nums[0]
-        while l<=r:
-            mid=(l+r)//2
+            if count<p:
+                return False
+            return True
+
+        low, high = 0, nums[-1]-nums[0]
+        while low<=high:
+            mid = (low+high)//2
             if isValid(mid):
-                ans=mid
-                r=mid-1
+                ans = mid
+                high=mid-1
             else:
-                l=mid+1
+                low = mid+1
         return ans
+            
