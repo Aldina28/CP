0412916e07c8a07744374888dced118c318c67dd
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-15 00:00:45 +0530

    Time: 49 ms (41.96%) | Memory: 19.5 MB (81.07%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -3,16 +3,21 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
             return 
-        seen = set()
-        while head:
-            if head not in seen:
-                seen.add(head)
-            else:
-                return head
-            head = head.next
-        return None
+        fast, slow = head,head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        else:
+            return None
+        fast = head
+        while fast!=slow:
+            fast = fast.next
+            slow = slow.next
+        return slow
