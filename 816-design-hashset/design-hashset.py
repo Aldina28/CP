14f240e0359737efcdc585ae277425ed73ec5e99
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-15 00:19:26 +0530

    Time: 59 ms (45.86%) | Memory: 44.5 MB (11.56%) - LeetSync

--- 816-design-hashset/design-hashset.py
+++ 816-design-hashset/design-hashset.py
@@ -1,18 +1,16 @@
 class MyHashSet:
 
     def __init__(self):
-        self.mp=[False]*1000001
-        
+        self.my_list = [0] * 1000001
 
     def add(self, key: int) -> None:
-        self.mp[key]=True
-        
+        self.my_list[key] = 1
 
     def remove(self, key: int) -> None:
-        self.mp[key]=False
+        self.my_list[key] = 0
 
     def contains(self, key: int) -> bool:
-        return self.mp[key]
+        return self.my_list[key] == 1
 
 
 # Your MyHashSet object will be instantiated and called as such:
