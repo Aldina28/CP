1df261a0d5b13c4860a20d998c4d1466bd8bf226
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-17 13:58:04 +0530

    Time: 4 ms (92.19%) | Memory: 26 MB (10.94%) - LeetSync

--- 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/count-the-number-of-arrays-with-k-matching-adjacent-elements.py
+++ 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/count-the-number-of-arrays-with-k-matching-adjacent-elements.py
@@ -1,14 +1,17 @@
+mx = 10**5 + 10
+mod = 10**9 + 7
+f = [1] + [0] * mx
+g = [1] + [0] * mx
+
+for i in range(1, mx):
+    f[i] = f[i - 1] * i % mod
+    g[i] = pow(f[i], mod - 2, mod)
+
+
+def comb(m: int, n: int) -> int:
+    return f[m] * g[n] * g[m - n] % mod
+
+
 class Solution:
     def countGoodArrays(self, n: int, m: int, k: int) -> int:
-        mod, d = 10**9 + 7, n - 1
-        if k > d:       return 0
-        r, C = k, 1
-        if r > d - r:   r = d - r
-        if r:
-            inv = [0] * (r + 1)
-            inv[1] = 1
-            for i in range(2, r + 1):
-                inv[i] = mod - (mod // i) * inv[mod % i] % mod
-            for i in range(1, r + 1):
-                C = C * (d - r + i) % mod * inv[i] % mod
-        return m * C % mod * pow(m - 1, d - k, mod) % mod
+        return comb(n - 1, k) * m * pow(m - 1, n - k - 1, mod) % mod
