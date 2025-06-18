78e5f1e29cf81b4f31e237f587dcf2bc35053504
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-18 10:28:59 +0530

    Time: 74 ms (88.61%) | Memory: 33.6 MB (6.33%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -3,8 +3,7 @@ class Solution:
         res = []
         nums.sort()
         for i in range(0, len(nums), 3):
-            if nums[i+2]-nums[i+1]<=k and nums[i+1]-nums[i]<=k and nums[i+2]-nums[i]<=k:
-                res.append([nums[i], nums[i+1], nums[i+2]])
-            else:
-                res = []
-        return res if len(res)==len(nums)//3 else []
+            if nums[i+2]-nums[i]>k:
+                return []
+            res.append([nums[i], nums[i+1], nums[i+2]])
+        return res 
