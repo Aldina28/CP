5910ce909c664cbae7af2ae8b26c827358a35e9a
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-18 17:31:00 +0530

    Time: 135 ms (45.59%) | Memory: 17.7 MB (65.83%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -7,7 +7,6 @@ class Solution:
                 count+=1
                 l-=1
                 r+=1
-
             l, r = i, i+1
             while l>=0 and r<len(s) and s[l]==s[r]:
                 count+=1
