e7b65ac04ab4fa4c35abb66c1173f5bc7a13af77
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-20 12:53:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (61.13%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -11,7 +11,7 @@ class Solution:
             mid = (l+r)//2
             if nums[mid+1]<nums[mid] and nums[mid-1]<nums[mid]:
                 return mid
-            elif nums[mid+1]>nums[mid]:
-                l = mid + 1
             elif nums[mid+1]<nums[mid]:
                 r = mid-1
+            elif nums[mid+1]>nums[mid]:
+                l = mid+1
