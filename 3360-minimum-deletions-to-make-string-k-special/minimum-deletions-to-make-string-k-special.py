7f840b767daf62f5aabd0020310796645f3b324c
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-21 21:10:00 +0530

    Time: 59 ms (62.79%) | Memory: 18.1 MB (45.74%) - LeetSync

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.py
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.py
@@ -1,25 +1,12 @@
 class Solution:
     def minimumDeletions(self, word: str, k: int) -> int:
-        freqMap = defaultdict(int)
-        for c in word:
-            freqMap[c] += 1
+        A = Counter(word).values()
+        res = inf
         
-        frequencies = sorted(freqMap.values())
-        minDeletions = float('inf')
-        n = len(frequencies)
+        for x in A:
+            cur = 0
+            for a in A:
+                cur += a if a < x else max(0, a - (x + k))
+            res = min(res, cur)
         
-        for i in range(n):
-            base = frequencies[i]
-            totalDeletions = 0
-            
-            for j in range(i):
-                totalDeletions += frequencies[j]
-            
-            for j in range(i, n):
-                if frequencies[j] > base + k:
-                    totalDeletions += frequencies[j] - (base + k)
-            
-            if totalDeletions < minDeletions:
-                minDeletions = totalDeletions
-        
-        return minDeletions
+        return res
