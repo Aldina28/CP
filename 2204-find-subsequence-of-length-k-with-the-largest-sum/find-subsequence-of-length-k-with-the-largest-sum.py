61dd0bf72292e851a7a88781cfbf730dad4c4a85
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-28 21:00:04 +0530

    Time: 4 ms (48.81%) | Memory: 18.1 MB (54.89%) - LeetSync

---
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        nums_with_indices = [(num, i) for i, num in enumerate(nums)]
+        
+        # Sort by value descending
+        nums_with_indices.sort(key=lambda x: -x[0])
+        
+        # Take top k and sort by original index
+        top_k = sorted(nums_with_indices[:k], key=lambda x: x[1])
+        
+        # Extract values
+        return [num for num, _ in top_k]
