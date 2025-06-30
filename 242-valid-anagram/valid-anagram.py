6e1e61715d5b7bf309c43b704b86a37bf41e5ac6
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-06-30 19:53:23 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (63.62%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,9 +1,12 @@
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        if len(s)!=len(t):
             return False
-        for c in set(s):
-            if t.count(c) != s.count(c):
+        for i in s:
+            if i in t:
+                if s.count(i)!=t.count(i):
+                    return False
+            else:
                 return False
         return True
