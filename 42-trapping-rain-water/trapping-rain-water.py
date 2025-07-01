b7024a5182b6bd815939079ba20961b841b94338
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-07-01 22:29:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (92.88%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -4,17 +4,22 @@ class Solution:
         r = len(height)-1
         left_max = height[l]
         right_max = height[r]
-        water = 0
+        area=0
         while l<r:
-            if left_max<right_max:
+            if height[l]<height[r]:
+                if height[l]>=left_max:
+                    left_max = height[l]
+                else:
+                    area += left_max - height[l]
                 l+=1
-                left_max = max(left_max, height[l])
-                water+=left_max-height[l]
             else:
+                if height[r]>=right_max:
+                    right_max = height[r]
+                else:
+                    area+=right_max - height[r]
                 r-=1
-                right_max = max(right_max, height[r])
-                water+=right_max-height[r]
-        return water
+        return area
+
 
 
 
