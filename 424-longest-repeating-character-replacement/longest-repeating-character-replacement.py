7d32f085b01de1d604587d66779cb4c31f502204
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-07-06 13:33:21 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (23.51%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,15 +1,15 @@
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        freq = defaultdict(int)
-        res = 0
-        i = 0
-        for j in range(len(s)):
-            freq[s[j]] += 1
-            max_freq = max(freq.values())
-            curr_len = j-i+1
-            if curr_len - max_freq>k:
-                freq[s[i]]-=1
-                i+=1
-            res = max(res, j-i+1)
-        return res
+        result = defaultdict(int)
+        l = 0
+        r = 0
+        max_len = 0
+        for r in range(len(s)):
+            result[s[r]] += 1
+            max_freq = max(result.values())
+            if (r-l+1) - max_freq>k:
+                result[s[l]]-=1
+                l+=1
+            max_len = max(max_len, r-l+1)
+        return max_len
