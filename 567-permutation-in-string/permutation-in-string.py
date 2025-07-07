040a4e4a169c2019a6b9d226b3d1f9614b4bdb21
Aldina28 <102737626+Aldina28@users.noreply.github.com>
2025-07-07 07:23:55 +0530

    Time: 11 ms (97.31%) | Memory: 18 MB (19.03%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,27 +1,24 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        n1 = len(s1)
-        n2 =len(s2)
-
-        if n1 > n2:
+        if len(s1)>len(s2):
             return False
+        
+        n1= len(s1)
+        n2 = len(s2)
 
-        count1 = [0]*26
-        count2 = [0]*26
-        window =set()
+        array1 = [0]*26
+        array2 = [0]*26
 
-        for i in range (n1):
-            count1[ord(s1[i]) -97] +=1
-            count2[ord(s2[i]) -97] +=1
-
-        if count1 == count2:
+        for i in range(n1):
+            array1[ord(s1[i])-97]  +=1
+            array2[ord(s2[i])-97] +=1
+        
+        if array1 == array2:
             return True
-
-        for i in range (n1, n2):
-             count2[ord(s2[i]) -97] +=1
-             count2[ord(s2[i-n1]) -ord('a')] -=1
-
-             if count1 == count2:
+        
+        for i in range(n1, n2):
+            array2[ord(s2[i])-97] +=1
+            array2[ord(s2[i-n1])-ord('a')]-=1
+            if array1==array2:
                 return True
-            
         return False
