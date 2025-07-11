9ed7861e2f0c541c6cd913dbe1b13bef51739ef2
Romano Aldina <102737626+Aldina28@users.noreply.github.com>
2025-07-11 08:29:12 +0530

    Time: 9 ms (19.90%) | Memory: 19.1 MB (25.81%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.py
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -1,16 +1,27 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
         res = []
-        def backtrack(i, temp):
-            if i>=len(s):
-                res.append(temp)
-                return 
-            
+
+        def backtrack(i, path):
+            if i >= len(s):
+                res.append(''.join(path))
+                return
+
             if s[i].isalpha():
-                backtrack(i+1, temp+s[i].upper())
-                backtrack(i+1, temp+s[i].lower())
+                # Choose lowercase
+                path.append(s[i].lower())
+                backtrack(i + 1, path)
+                path.pop()  # undo
+
+                # Choose uppercase
+                path.append(s[i].upper())
+                backtrack(i + 1, path)
+                path.pop()  # undo
+
             else:
-                backtrack(i+1, temp+s[i])
-        temp = ''
-        backtrack(0, temp)
+                path.append(s[i])
+                backtrack(i + 1, path)
+                path.pop()  # undo
+
+        backtrack(0, [])
         return res
