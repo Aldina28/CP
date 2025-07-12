f47da9733eeadcd9805f8566b61579c1657dfebb
Romano Aldina <102737626+Aldina28@users.noreply.github.com>
2025-07-12 10:53:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (61.70%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -5,13 +5,14 @@ class Solution:
         temp = []
         def backtrack(i, curr_sum):
             if curr_sum == target:
-                res.append(temp[:])
-                return
-            if i >= len(candidates) or curr_sum > target:
-                return
+                res.append(temp.copy())
+                return 
+            if i>=len(candidates) or curr_sum>target:
+                return 
             temp.append(candidates[i])
-            backtrack(i, curr_sum + candidates[i])
+            backtrack(i, curr_sum+candidates[i])
             temp.pop()
-            backtrack(i + 1, curr_sum)
+            backtrack(i+1, curr_sum)
         backtrack(0, 0)
         return res
+            
