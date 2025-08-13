95189684751c87e1f958cab6069ab01f9970735e
Romano Aldina <102737626+Aldina28@users.noreply.github.com>
2025-08-13 20:04:46 +0530

    Time: 12 ms (42.15%) | Memory: 17.7 MB (72.35%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -1,9 +1,8 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        if n <= 0:
-            return False
-        while n > 1:  
-            if n % 3 != 0:  
+        if n<=0: return False
+        while n>1:
+            if n%3==0: n=n//3
+            else:
                 return False
-            n = n // 3
         return True
