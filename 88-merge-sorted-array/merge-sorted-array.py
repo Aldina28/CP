1fe3825eeca49f00f0bc4b7b99693347e1340c91
Romano Aldina <102737626+Aldina28@users.noreply.github.com>
2025-09-12 08:54:19 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (99.34%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -5,14 +5,14 @@ class Solution:
         """
         m_idx = m-1
         n_idx = n-1
-        ptr = m+n-1
+        p = m+n-1
         while n_idx>=0:
-            if m_idx>=0 and nums1[m_idx] > nums2[n_idx]:
-                nums1[ptr] = nums1[m_idx]
+            if m_idx>=0 and nums1[m_idx]>nums2[n_idx]:
+                nums1[p] = nums1[m_idx]
+                p-=1
                 m_idx-=1
-                ptr-=1
             else:
-                nums1[ptr] = nums2[n_idx]
-                n_idx-=1
-                ptr-=1   
-
+                nums1[p] = nums2[n_idx]
+                p-=1
+                n_idx -=1
+    
